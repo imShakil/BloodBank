9a524064c7d81232bc215093fc10fbbbdc6a1615
mehbuba <minu1807120@stud.kuet.ac.bd>
2022-05-02 02:17:39 +0600

    prac

---
+++ app/src/main/java/com/android/iunoob/bloodbank/assignment_git_newFile.java
@@ -0,0 +1,15 @@
+package com.android.iunoob.bloodbank;
+
+public class assignment_git_newFile {
+    private int num;
+
+    public assignment_git_newFile(int x) {
+     num=x;
+    }
+    public void setNum(int x){
+        num=x;
+    }
+    public int getNum(){
+        return num;
+    }
+}
