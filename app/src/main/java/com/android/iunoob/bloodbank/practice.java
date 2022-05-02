9059a7da322220f9002b3b8e7168cfe6212ac5a7
mehbuba <minu1807120@stud.kuet.ac.bd>
2022-05-02 12:20:20 +0600

    practice_work

---
+++ app/src/main/java/com/android/iunoob/bloodbank/practice.java
@@ -0,0 +1,15 @@
+package com.android.iunoob.bloodbank;
+
+public class practice {
+    private int num;
+    public practice(int x){
+        num=x;
+    }
+    public void setNum(int x){
+        num=x;
+    }
+
+    public int getNum() {
+        return num;
+    }
+}
