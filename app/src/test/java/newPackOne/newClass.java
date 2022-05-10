4f401783285c38ee88641e2655a1f23c0723f5b7
MAZHAR1807102 <imazharul175@gmail.com>
2022-05-10 20:59:16 +0600

    newClass

---
+++ app/src/test/java/newPackOne/newClass.java
@@ -0,0 +1,27 @@
+package newPackOne;
+
+public class newClass {
+    public newClass(int a, String s) {
+        this.a = a;
+        S = s;
+    }
+
+    int a;
+    String S;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getS() {
+        return S;
+    }
+
+    public void setS(String s) {
+        S = s;
+    }
+}
