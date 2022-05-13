dca62cb247e30a1205f92bd08f9a490fd14a5755
Sumaiya Mifra <abc@gmail.com>
2022-05-13 23:36:39 +0600

    branch file

---
+++ app/src/main/java/com/android/iunoob/bloodbank/activities/branch_file.java
@@ -0,0 +1,21 @@
+public class branch_file {
+    public branch_file() {
+    }
+    EditText text1, text2;
+
+    public EditText getText1() {
+        return text1;
+    }
+
+    public void setText1(EditText text1) {
+        this.text1 = text1;
+    }
+
+    public EditText getText2() {
+        return text2;
+    }
+
+    public void setText2(EditText text2) {
+        this.text2 = text2;
+    }
+}
