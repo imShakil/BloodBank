225d895ab8c3ef900b14ee0d1a619322b8fab62e
MotasemZaidAlkilani <m.kilany1999@gmail.com>
2022-06-13 23:12:10 +0300

    fix gradle issues

--- app/src/main/java/com/android/iunoob/bloodbank/activities/LoginActivity.java
+++ app/src/main/java/com/android/iunoob/bloodbank/activities/LoginActivity.java
@@ -2,9 +2,9 @@ package com.android.iunoob.bloodbank.activities;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
