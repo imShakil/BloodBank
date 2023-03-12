4d759ae5ed3822f150961a8c2da0340b2570aeab
Labib <abubuckkersiddikh@gmail.com>
2023-03-12 03:51:00 -0600

    ok

---
+++ practice1.java
@@ -0,0 +1,13 @@
+public class Vehicle {
+    private String color;
+
+    // Getter
+    public String getColor() {
+        return color;
+    }
+
+    // Setter
+    public void setColor(String c) {
+        this.color = c;
+    }
+}
