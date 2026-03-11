a88b9d082412811848b0f3aa4600ad12fe96a56b
Global Mobarak <mh.ice.iu@gmail.com>
2026-03-11 20:19:55 +0600

    fix viteconfigs

--- web-admin/vite.config.ts
+++ web-admin/vite.config.ts
@@ -2,5 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'firebase-vendor': ['firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/database']
+        }
+      }
+    }
+  }
 });
